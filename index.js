0d6b65eb8000b5866b36f3067deed2715f93c868
10krishnad <125298029+10krishnad@users.noreply.github.com>
2023-04-08 01:43:13 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,31 +1,69 @@
+//const bg_sound = new Audio("./bg_sound.wav");
+//bg_sound.play();
 const playerImg = document.getElementById("player-img");
-console.log(playerImg);
-const a = playerImg.getBoundingClientRect();
-console.log(a.x);
+const container = document.getElementsByClassName("container");
+const c_width = container[0].offsetWidth;
+const c_height = container[0].offsetHeight;
 
-addEventListener("keydown",button);
+window.addEventListener("load", () => {
+    playerImg.style.position = "absolute";
+    playerImg.style.left = 0;
+    playerImg.style.top = 0;
+});
+window.addEventListener("keyup",(e) => {
+    switch (e.key){
+        case "ArrowLeft":
+            const left = parseInt(playerImg.style.left);
+            playerImg.src="./images/left.png";
+            move_Zombie();
+            console.log(left);
+            if (left > 0)
+            playerImg.style.left = left - 50 + "px";
+            break;
+        case "ArrowRight":
+            const right = parseInt(playerImg.style.left);
+            playerImg.src="./images/right.png";
+            const x = playerImg.getBoundingClientRect().x;
+            if (x < c_width){
+            console.log(c_width,x);
+            playerImg.style.left = right + 50 + "px";
+            }
+            break;
+        case "ArrowUp":
+            const top = parseInt(playerImg.style.top);
+            playerImg.src="./images/up.png";
+            move_Zombie();
+            if (top > 0)
+            playerImg.style.top = top - 50 + "px";
+            break;
+        case "ArrowDown":
+            const bottom = parseInt(playerImg.style.top);
+            playerImg.src="./images/down.png";
+            const y = playerImg.getBoundingClientRect().y;
+            if (y < c_height){
+            playerImg.style.top = bottom + 50 + "px";
+            }
+            break;
+    }
+});
+    const img = document.createElement("img");
+    img.src = "./images/zleft.png";
+    img.style.position= "absolute"
+    img.style.width = 250 +"px";
+    img.style.height = 250 +"px";
+    container[0].appendChild(img);
 
-function button(event){
-    if(event.key === "ArrowRight"){
-        const right = parseInt(playerImg.style.left);
-        console.log(right);
-        playerImg.style.left = right + 20 + "px";
-    }
-    else if (event.key === "ArrowLeft"){
-        const right = parseInt(playerImg.style.left);
-        playerImg.style.left = right -20 + "px";
-    }
-    else if (event.key === "ArrowUp"){
-        const top = parseInt(playerImg.style.top);
-        console.log(top);
-        playerImg.style.top =  top + 20 + "px";
-    }
-    else if (event.key === "ArrowDown"){
-        const top = parseInt(playerImg.style.top);
-        console.log(top);
-        playerImg.style.top = top - 20 + "px";
-    }
-    else{
-        console.log(event.key);
-    }
+function move_Zombie(){
+    const left = playerImg.getBoundingClientRect().x;
+    const top = playerImg.getBoundingClientRect().y;
+    const left_z = parseInt(img.style.left);
+    const top_z = parseInt(img.style.top);
+    console.log(top_z,left_z);
+    img.style.top = top_z + 20 + "px";
+    img.style.left = left_z + 20 + "px";
 }
+function animate() {
+        requestAnimationFrame(animate);
+        move_Zombie();
+}
+animate();
